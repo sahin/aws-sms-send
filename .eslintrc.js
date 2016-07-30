4629f2043e406f6c0c0d9dd6465307d07b078f2d
ccali14 <calicagatay@icloud.com>
2016-07-29 20:10:48 -0700

    Readme improved, demo added

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,10 @@
 module.exports = {
-  "parser": "babel-eslint",
-  "rules": {
-    "strict": 0
+  parser: 'babel-eslint',
+  rules: {
+    strict: 0,
   },
-  "extends": "airbnb",
-  "plugins": [
-    "react"
-  ],
-  "rules": {
-    "no-console": 0
+  extends: 'airbnb',
+  rules: {
+    'no-console': 0
   }
 };
